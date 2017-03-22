1025990e9a25e3e45133a038cfe373bc89ee0551
Jack Schluger <jschluger@stuy.edu>
2017-03-22 14:01:25 -0400

    class work

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var nums = [4, 8, 15, 18, 16, 23, 42]
+var chart =  d3.select(".chart"); 
 
 var bars = function() {
-    d3.select(".chart")
+    chart
 	.selectAll("div")
 	.data( nums )
 	.enter().append("div")
@@ -11,3 +12,12 @@ var bars = function() {
 };
 
 bars();
+
+var myTransition = function(m) {
+    chart.selectAll('div')
+	.data(nums)
+	.transition()
+	.style('width', function(d) {
+	    return d * m + "px";
+	});
+};
