d449c6eba4b5d4e66d1667002a3c842ca4aca282
Jack Schluger <jschluger@stuy.edu>
2017-03-22 01:52:07 -0400

    f

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var nums = [4, 8, 15, 18, 16, 23, 42]
 
-d3.select(".chart")
-    .selectAll("div")
-    .data( nums )
-    .enter().append("div")
-    .style("width", function(d) {
-	return d*10 + "px"; })
-    .text(function(d) {return d;} );
+var bars = function() {
+    d3.select(".chart")
+	.selectAll("div")
+	.data( nums )
+	.enter().append("div")
+	.style("width", function(d) {
+	    return d*10 + "px"; })
+	.text(function(d) {return d;} );
+};
+
+bars();
