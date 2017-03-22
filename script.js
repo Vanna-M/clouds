15f85105891f59c3a49fcef66d6c83ca697960af
Jack Schluger <jschluger@stuy.edu>
2017-03-21 21:43:50 -0400

    adding code from class

---
+++ script.js
@@ -0,0 +1,9 @@
+var nums = [4, 8, 15, 18, 16, 23, 42]
+
+d3.select(".chart")
+    .selectAll("div")
+    .data( nums )
+    .enter().append("div")
+    .style("width", function(d) {
+	return d*10 + "px"; })
+    .text(function(d) {return d;} );
